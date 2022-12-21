56839a8e3aa5e952c90c33e98b1ff35ffac0323c
Hafizur Rahman Fahim <hafijurrahman046@gmail.com>
2022-12-21 21:57:56 +0600

    refactoring

--- index.js
+++ index.js
@@ -47,73 +47,74 @@ omegle.eventEmitter.on("stoppedTyping", () => {
 });
 
 //handle frontend events from socket.io
+async function messageHandler(message) {
+  io.emit("message", `You: ${message}`);
+  if (message === "!help") {
+    io.emit(
+      "message",
+      "available commands: !status `!set $interest` !start !end !kill !stop"
+    );
+  }
+  if (message === "!status") {
+    io.emit("message", "---------------------------------------------");
+    io.emit(
+      "message",
+      `interest: ${omegle.interest}, enabled: ${!omegle.noInterest}`
+    );
+    io.emit("message", ` applicationEnabled: ${omegle.applicationEnabled}`);
+    io.emit("message", "---------------------------------------------");
+    return;
+  }
+  if (message === "!start") {
+    omegle.applicationEnabled = true;
+    await omegle.connect();
+    return;
+  }
+  if (message === "!togglein") {
+    omegle.noInterest = !omegle.noInterest;
+    io.emit("message", "---------------------------------------------");
+    io.emit(
+      "message",
+      `Interest ${omegle.noInterest ? "disabled" : "enabled"}!`
+    );
+    io.emit("message", "---------------------------------------------");
+    return;
+  }
+  if (message.split(" ")[0] === "!set") {
+    omegle.interest = message.split(" ")[1];
+    io.emit("message", "---------------------------------------------");
+    io.emit("message", `API: Current interest is ${omegle.interest}`);
+    io.emit("message", "---------------------------------------------");
+    return;
+  }
+  if (message === "!greeting") {
+    io.emit("message", `You: ${omegle.starterMessage}`);
+    return;
+  }
+  if (message.split(" ")[0] === "!greeting") {
+    let wordArray = message.split(" ");
+    wordArray.shift();
+    omegle.starterMessage = wordArray.reduce((prev, current) => {
+      return prev + " " + current;
+    });
+    io.emit("message", "---------------------------------------------");
+    io.emit("message", `API: Current greeting is "${omegle.starterMessage}"`);
+    io.emit("message", "---------------------------------------------");
+    return;
+  }
+  if (message === "!end") {
+    omegle.applicationEnabled = false;
+    return;
+  }
+  if (message === "!kill") {
+    await omegle.disconnect();
+    io.emit("message", "You have disconnected.");
+    return;
+  }
+  await omegle.sendMessage(message);
+}
 io.on("connection", (socket) => {
-  socket.on("message", async (message) => {
-    io.emit("message", `You: ${message}`);
-    if (message === "!help") {
-      io.emit(
-        "message",
-        "available commands: !status `!set $interest` !start !end !kill !stop"
-      );
-    }
-    if (message === "!status") {
-      io.emit("message", "---------------------------------------------");
-      io.emit(
-        "message",
-        `interest: ${omegle.interest}, enabled: ${!omegle.noInterest}`
-      );
-      io.emit("message", ` applicationEnabled: ${omegle.applicationEnabled}`);
-      io.emit("message", "---------------------------------------------");
-      return;
-    }
-    if (message === "!start") {
-      omegle.applicationEnabled = true;
-      await omegle.connect();
-      return;
-    }
-    if (message === "!togglein") {
-      omegle.noInterest = !omegle.noInterest;
-      io.emit("message", "---------------------------------------------");
-      io.emit(
-        "message",
-        `Interest ${omegle.noInterest ? "disabled" : "enabled"}!`
-      );
-      io.emit("message", "---------------------------------------------");
-      return;
-    }
-    if (message.split(" ")[0] === "!set") {
-      omegle.interest = message.split(" ")[1];
-      io.emit("message", "---------------------------------------------");
-      io.emit("message", `API: Current interest is ${omegle.interest}`);
-      io.emit("message", "---------------------------------------------");
-      return;
-    }
-    if (message === "!greeting") {
-      io.emit("message", `You: ${omegle.starterMessage}`);
-      return;
-    }
-    if (message.split(" ")[0] === "!greeting") {
-      let wordArray = message.split(" ");
-      wordArray.shift();
-      omegle.starterMessage = wordArray.reduce((prev, current) => {
-        return prev + " " + current;
-      });
-      io.emit("message", "---------------------------------------------");
-      io.emit("message", `API: Current greeting is "${omegle.starterMessage}"`);
-      io.emit("message", "---------------------------------------------");
-      return;
-    }
-    if (message === "!end") {
-      omegle.applicationEnabled = false;
-      return;
-    }
-    if (message === "!kill") {
-      await omegle.disconnect();
-      io.emit("message", "You have disconnected.");
-      return;
-    }
-    await omegle.sendMessage(message);
-  });
+  socket.on("message", messageHandler);
   socket.on("start-typing", async () => {
     await omegle.startTyping();
   });
